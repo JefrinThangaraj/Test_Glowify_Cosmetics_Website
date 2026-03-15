a500922f54be1697070c57a30863d69ac63032eb
JefrinThangaraj <jefrinthangaraj01@gmail.com>
2026-03-15 21:52:22 +0530

    Resolved merge conflict

--- locators/login_locator.py
+++ locators/login_locator.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 class LoginLocator:
 
-    NAME_INPUT = (By.ID, "name")
+    NAME_INPUT = (By.NAME, "name")
     EMAIL_INPUT = (By.ID, "email")
     PASSWORD_INPUT = (By.ID, "password")
     REMEMBER_ME = (By.ID, "remember")
